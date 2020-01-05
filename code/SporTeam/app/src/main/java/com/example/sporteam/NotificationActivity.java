238cb6a67d7ff659b3f649957c7d180ef8862f28
Sonia Grigor <sonia.grigor@gmail.com>
2020-01-05 16:33:11 +0200

    Finish Notification activity and showing notification

--- code/SporTeam/app/src/main/java/com/example/sporteam/NotificationActivity.java
+++ code/SporTeam/app/src/main/java/com/example/sporteam/NotificationActivity.java
@@ -1,20 +1,155 @@
 package com.example.sporteam;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 public class NotificationActivity extends AppCompatActivity {
+
+    Switch hydrationSwitch;
+    Switch weatherSwitch;
+    Switch nutritionSwitch;
+    public static final String CHANNEL_ID = "com.example.sporteam.ANDROID";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
 
+        hydrationSwitch = findViewById(R.id.chip_hidratare);
+        weatherSwitch = findViewById(R.id.chip_vreme);
+        nutritionSwitch = findViewById(R.id.chip_alimentatie);
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "sporteam", importance);
+            channel.setDescription("our project for piu");
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
     @Override
     public void onBackPressed() {
         finish();
     }
+
+    private NotificationManager notifManager;
+
+    public void createNotification(String aMessage, Context context, Class nextClass) {
+
+        final int NOTIFY_ID = 0; // ID of notification
+        String id = "default_channel_id"; // default_channel_id
+        String title = "title"; // Default Channel
+        Intent intent;
+        PendingIntent pendingIntent;
+        NotificationCompat.Builder builder;
+        if (notifManager == null) {
+            notifManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = notifManager.getNotificationChannel(id);
+            if (mChannel == null) {
+                mChannel = new NotificationChannel(id, title, importance);
+                mChannel.enableVibration(true);
+                mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+                notifManager.createNotificationChannel(mChannel);
+            }
+            builder = new NotificationCompat.Builder(context, id);
+            intent = new Intent(context, nextClass);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            builder.setContentTitle(context.getString(R.string.app_name))                            // required
+                    .setSmallIcon(android.R.drawable.ic_popup_reminder)   // required
+                    .setContentText(aMessage) // required
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+                    .setTicker(aMessage)
+                    .setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+        }
+        else {
+            builder = new NotificationCompat.Builder(context, id);
+            intent = new Intent(context, nextClass);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            builder.setContentTitle(context.getString(R.string.app_name))                            // required
+                    .setSmallIcon(android.R.drawable.ic_popup_reminder)   // required
+                    .setContentText(aMessage) // required
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+                    .setTicker(aMessage)
+                    .setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400})
+                    .setPriority(Notification.PRIORITY_HIGH);
+        }
+        Notification notification = builder.build();
+        notifManager.notify(NOTIFY_ID, notification);
+    }
+
+    public void startNotificationHydration(View view) {
+        new CountDownTimer(3000, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+            }
+
+            @Override
+            public void onFinish() {
+               createNotification("Nu uita sa te hidratezi", NotificationActivity.this, NotificationActivity.class);
+            }
+        }.start();
+
+    }
+
+    public void startNotificationWeather(View view) {
+        new CountDownTimer(1000, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+            }
+
+            @Override
+            public void onFinish() {
+                createNotification("Vremea e minunata pentru sport. Alege ce vrei sa faci maine.", NotificationActivity.this, MainActivity.class);
+            }
+        }.start();
+    }
+
+    public void startNotificationNutrition(View view) {
+        new CountDownTimer(5000, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+            }
+
+            @Override
+            public void onFinish() {
+                createNotification("Consulta planul tau personalizat de  nutritie", NotificationActivity.this, NotificationActivity.class);
+            }
+        }.start();
+
+    }
+
 }
